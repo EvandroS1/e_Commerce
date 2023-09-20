434a8d6742e805d3575df3a55de3176684be8589
Evandro <evandro.gomes@actionsys.com.br>
2023-09-20 16:20:42 -0300

    retornando da api as informações, porem retorna tudo em um card, tem que individualizar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import NavBar from "./components/NavBar";
+import { Provider } from 'react-redux';
+
 import TenisCard from "./components/TenisCard"
+
+import RepositoryList from "./components/RepositoryList";
+import store from "./store";
 
 const App = () => {
 
   return (
   <div>
     <NavBar />
+    <Provider store={store}>
+      <RepositoryList />
+    </Provider>;
     <div className="grid place-items-center mt-20 w-full gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <Provider store={store}>
     <TenisCard />
-    <TenisCard />
-    <TenisCard />
-    <TenisCard />
-    <TenisCard />
-    <TenisCard />
-    <TenisCard />
-    <TenisCard />
-    <TenisCard />
-    <TenisCard />
-    <TenisCard />
-    <TenisCard />
+    </Provider>;
     </div>
 
   </div>
--- src/components/TenisCard.tsx
+++ src/components/TenisCard.tsx
@@ -1,16 +1,57 @@
-const TenisCard = () => {
-  return (
-    <div className="card w-96 glass">
-  <figure><img src="sla" alt="car!"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">Life hack</h2>
-    <p>How to park your car at your garage?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Learn now!</button>
-    </div>
-  </div>
-</div>
-  )
+import { Component } from "react";
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
+
+import { ApplicationState } from "@/store";
+import { Repository } from "@/store/ducks/repositories/types";
+import * as RepositoriesActions from "@/store/ducks/repositories/actions";
+
+interface StateProps {
+  repositories: Repository[]
 }
 
-export default TenisCard
+interface DispatcheProps {
+  loadRequest(): void
+}
+
+type Props = StateProps & DispatcheProps
+
+class TenisCard extends Component<Props> {
+  componentDidMount() {
+    const { loadRequest } = this.props;
+    loadRequest()
+  }
+
+  render() {
+    const { repositories } = this.props;
+
+    return (
+      <div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+      <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+      <div className="card-body">
+        <h2 className="card-title">
+        {repositories.map(repository => repository.title)}
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p>{repositories.map(repository => repository.description)}</p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">{repositories.map(repository => repository.category)}</div> 
+          <div className="badge badge-outline">Products</div>
+        </div>
+      </div>
+    </div>
+      </div>
+      
+    )
+  }
+}
+
+const mapStateToProps = (state: ApplicationState) => ({
+  repositories: state.repositories.data,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) =>
+bindActionCreators(RepositoriesActions, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(TenisCard);
