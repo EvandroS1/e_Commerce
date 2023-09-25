510bf4f58e15453952bc5321ebc5b2726b9728ee
Evandro <evandro.gomes@actionsys.com.br>
2023-09-25 18:32:06 -0300

    refatoração, adicionando onstate

---
+++ src/components/Loading.tsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+  return (
+    <div className="absolute top-96">
+      <span className="loading loading-spinner loading-lg"></span>
+
+    </div>
+  )
+};
+
+export default Loading;
